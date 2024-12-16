8562bdc6b52d92127c1266f2cae5a930532b1bb7
Tulsi <viranitulsi153@gmail.com>
2024-12-16 14:34:27 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/change_app_icon/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.change_app_icon
+
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:change_app_icon/change_icon.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      const MaterialApp(
+        home: MyApp(),
+      ),
+    );
